6887d8de985c519c8640d49a16fec091633d7d9c
qedized <qedized@gmail.com>
2022-01-01 00:34:39 -0500

    ban.js finished
    Copied and pasted kick.js basically.

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -1,30 +1,32 @@
 const Command = require('../../structures/Command.js');
+const Discord = require('discord.js');
 
-function kickMember(member, message) {
+function banMember(member, message) {
     var username = member.user.username;
-    if(member.kickable) {
-        member.kick();
-        message.channel.send(username + " was kicked successfully.");
+    if(member.bannable) {
+        member.ban();
+        message.channel.send(username + " was banned successfully.");
     } else {
-        message.channel.send("Can't kick " + username + "!");
+        message.channel.send("Can't ban " + username + "!");
     }
 }
 
 module.exports = new Command({
-	name: 'kick',
-	description: 'Kick a user or multiple users.',
-	permission: 'KICK_MEMBERS',
+	name: 'ban',
+	description: 'Ban a member or multiple members.',
+	permission: 'BAN_MEMBERS',
 
 	async run(message, args, client) {
+
 		if(args.length == 1 || (args.length >= 3 && message.mentions.users.size < args.length - 1)) { // no parameters or parameters without users
-			message.reply('Correct usage `os!kick [user_tag]+`');
+			message.reply('Correct usage `os!ban [user_tag]+`');
         } else if (message.mentions.users.size === 0) { // no tags
             message.channel.send('You did not tag any user. (or they are not in this server right now.)');
         } else if (args.length == 2 && message.mentions.users.size === 1) { // one tag
-            kickMember(message.mentions.members.first(), message);
+            banMember(message.mentions.members.first(), message);
         } else { // more than 1 user (and all parameters are users)
-            for (let i = 0; i < message.mentions.members.size; i++) { // kick the members
-                kickMember(message.mentions.members.at(i), message);
+            for (let i = 0; i < message.mentions.members.size; i++) { // ban the members
+                banMember(message.mentions.members.at(i), message);
             }
         }
 	}
