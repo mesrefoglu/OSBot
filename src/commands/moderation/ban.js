47aadaf11cf9b137919acc186055f10c3638acba
qedized <qedized@gmail.com>
2022-01-01 01:09:45 -0500

    updated kick and ban
    No more self kick and kicking people higher than you.

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -1,32 +1,35 @@
 const Command = require('../../structures/Command.js');
-const Discord = require('discord.js');
 
-function banMember(member, message) {
+function banMember(member, self, message) {
     var username = member.user.username;
-    if(member.bannable) {
+    if (member == self) {
+        message.channel.send("You can't ban yourself!");
+    } else if (member.roles.highest.position > self.roles.highest.position) {
+        message.channel.send("You can't ban " + username + " because of role hierarchy.");
+    } else if(!member.bannable) {
+        message.channel.send("Can't ban " + username + "!");
+    } else {
         member.ban();
         message.channel.send(username + " was banned successfully.");
-    } else {
-        message.channel.send("Can't ban " + username + "!");
     }
 }
 
 module.exports = new Command({
 	name: 'ban',
-	description: 'Ban a member or multiple members.',
+	description: 'Ban a user or multiple users.',
 	permission: 'BAN_MEMBERS',
 
 	async run(message, args, client) {
-
-		if(args.length == 1 || (args.length >= 3 && message.mentions.users.size < args.length - 1)) { // no parameters or parameters without users
+        // no parameters or parameters without users
+		if(args.length == 1 || (args.length >= 3 && message.mentions.users.size < args.length - 1)) {
 			message.reply('Correct usage `os!ban [user_tag]+`');
         } else if (message.mentions.users.size === 0) { // no tags
             message.channel.send('You did not tag any user. (or they are not in this server right now.)');
         } else if (args.length == 2 && message.mentions.users.size === 1) { // one tag
-            banMember(message.mentions.members.first(), message);
+            banMember(message.mentions.members.first(), message.member, message);
         } else { // more than 1 user (and all parameters are users)
             for (let i = 0; i < message.mentions.members.size; i++) { // ban the members
-                banMember(message.mentions.members.at(i), message);
+                banMember(message.mentions.members.at(i), message.member, message);
             }
         }
 	}
--- src/commands/moderation/kick.js
+++ src/commands/moderation/kick.js
@@ -1,12 +1,16 @@
 const Command = require('../../structures/Command.js');
 
-function kickMember(member, message) {
+function kickMember(member, self, message) {
     var username = member.user.username;
-    if(member.kickable) {
+    if (member == self) {
+        message.channel.send("You can't kick yourself!");
+    } else if (member.roles.highest.position > self.roles.highest.position) {
+        message.channel.send("You can't kick " + username + " because of role hierarchy.");
+    } else if(!member.kickable) {
+        message.channel.send("Can't kick " + username + "!");
+    } else {
         member.kick();
         message.channel.send(username + " was kicked successfully.");
-    } else {
-        message.channel.send("Can't kick " + username + "!");
     }
 }
 
@@ -16,15 +20,16 @@ module.exports = new Command({
 	permission: 'KICK_MEMBERS',
 
 	async run(message, args, client) {
-		if(args.length == 1 || (args.length >= 3 && message.mentions.users.size < args.length - 1)) { // no parameters or parameters without users
+        // no parameters or parameters without users
+		if(args.length == 1 || (args.length >= 3 && message.mentions.users.size < args.length - 1)) {
 			message.reply('Correct usage `os!kick [user_tag]+`');
         } else if (message.mentions.users.size === 0) { // no tags
             message.channel.send('You did not tag any user. (or they are not in this server right now.)');
         } else if (args.length == 2 && message.mentions.users.size === 1) { // one tag
-            kickMember(message.mentions.members.first(), message);
+            kickMember(message.mentions.members.first(), message.member, message);
         } else { // more than 1 user (and all parameters are users)
             for (let i = 0; i < message.mentions.members.size; i++) { // kick the members
-                kickMember(message.mentions.members.at(i), message);
+                kickMember(message.mentions.members.at(i), message.member, message);
             }
         }
 	}
